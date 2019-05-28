898c8c1bba303844943627aaa8371d7c9cdc4810
Valeria Vassallo <valevassallo1@gmail.com>
2019-05-28 16:49:33 -0500

    Use geolocation

--- src/index.js
+++ src/index.js
@@ -3,20 +3,27 @@ import { render } from "react-dom";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import "./style.css";
 
-const position = [-12.0711245, -77.0868577];
+function Geolocation() {
+  const [position, setPosition] = React.useState([-12, -77]);
+  navigator.geolocation.watchPosition(pos => {
+    setPosition([pos.coords.latitude, pos.coords.longitude]);
+  });
 
-const map = (
-  <Map center={position} zoom={13}>
-    <TileLayer
-      attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={position}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </Map>
-);
+  const map = (
+    <Map center={position} zoom={13}>
+      <TileLayer
+        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </Map>
+  );
 
-render(map, document.getElementById("root"));
+  return map;
+}
+
+render(<Geolocation />, document.getElementById("root"));
